5c586610b4d928ad013bd03fd2bb93a03e2efcb2
yashsakhla <yashsakhla33@gmail.com>
2025-07-30 00:45:32 +0530

    added fixes

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['https://sirius-perfumes.vercel.app', 'http://localhost:3000', 'http://localhost:3001',"https://vercel.com"],
+  origin: ['https://sirius-perfumes.vercel.app', 'http://localhost:3000', 'http://localhost:3001',"https://vercel.com", "https://sirius-perfumes-yckn.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
